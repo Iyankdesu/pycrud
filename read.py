c8e404cb1a4039d7a8e681c4dff9a72a29df2883
Muhammad Fajrian Nur Ibrahim <iyankdesu@gmail.com>
2024-02-13 15:53:36 +0800

    feat: print output as table

--- read.py
+++ read.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from prettytable import PrettyTable
 
 
 def read_todos(connection):
@@ -6,8 +7,10 @@ def read_todos(connection):
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM todo")
         todos = cursor.fetchall()
-        print("List:")
-        for todo in todos:
-            print(todo)
+        if todos:
+            table = PrettyTable(["ID", "Title", "Description", "Date"])
+            for todo in todos:
+                table.add_row(todo)
+            print(table)
     except mysql.connector.Error as err:
         print("Error:", err)
